705ed5666bcdc4f6c84a8bc1fea727793b6b7b8d
Alex Griessman <alex.griessman@gmail.com>
2015-11-21 17:15:21 -0500

    Added the binding energy header file.

---
+++ diffusivity/binding.h
@@ -0,0 +1,9 @@
+#ifndef BINDING_H
+#define BINDING_H
+
+double BindingEnergyGAB(gab*, double, double);
+double BindingEnergyOswin(oswin*, double, double);
+double BindingEnergyHenderson(henderson*, double, double);
+double BindingEnergyLitchfield(double, double);
+
+#endif
